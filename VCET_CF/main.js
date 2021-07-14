f0e73c4df7f35f30436cad01e0eef66f9cca43ad
Abhinivesh27 <77716914+Abhinivesh27@users.noreply.github.com>
2021-07-14 09:45:17 +0530

    Update main.js

--- VCET_CF/main.js
+++ VCET_CF/main.js
@@ -16,15 +16,15 @@ function manip() {
     var c12 = parseInt(document.getElementById("12_5").value);
     var b12 = parseInt(document.getElementById("12_6").value); 
 
-    var tt = parseFloat(ten_avg + t11 + t12,1);
-    var et = parseFloat(ten_avg + e11 + e12,1);
-    var mt = parseFloat(ten_avg + m11 + m12,1);
-    var pt = parseFloat(ten_avg + p11 + p12,1);
-    var ct = parseFloat(ten_avg + c11 + c12,1);
-    var bt = parseFloat(ten_avg + b11 + b12,1);
+    var tt = parseFloat(ten_avg + t11 + t12);
+    var et = parseFloat(ten_avg + e11 + e12);
+    var mt = parseFloat(ten_avg + m11 + m12);
+    var pt = parseFloat(ten_avg + p11 + p12);
+    var ct = parseFloat(ten_avg + c11 + c12);
+    var bt = parseFloat(ten_avg + b11 + b12);
 
-    var cutoff = (mt + pt/2 + ct/2).toFixed(2);
-    var total = ((ten_avg*6)+t11+e11+m11+p11+c11+b11+t12+e12+m12+p12+c12+b12).toFixed(1);
+    var cutoff = mt + pt/2 + ct/2;
+    var total = ((ten_avg*6)+t11+e11+m11+p11+c11+b11+t12+e12+m12+p12+c12+b12);
  console.log(cutoff,total);
 
  document.getElementById("username_view").innerHTML = username;
@@ -60,7 +60,7 @@ function manip() {
  document.getElementById("bt").innerHTML = Math.round(bt);
 
  document.getElementById("total").innerHTML = Math.ceil(total);
- document.getElementById("cutoff").innerHTML = cutoff;
+ document.getElementById("cutoff").innerHTML = cutoff.toFixed(2);
 
 }
 function validateForm() {
